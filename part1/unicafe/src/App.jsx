5a16030938c59fa4124109626fa3108ce269c399
Martin Winther <martin.winther@protonmail.com>
2023-10-23 09:41:01 +0200

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,24 +1,36 @@
 import { useState } from "react";
 
 const App = () => {
-	// save clicks of each button to its own state
+	// Save clicks of each button to its own state
 	const [good, setGood] = useState(0);
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
+	// Calculate the total number of feedback entries
+	const all = good + neutral + bad;
+
+	// Calculate the average score
+	// Check if 'all' is zero to avoid division by zero
+	const average = all === 0 ? 0 : (good - bad) / all;
+
+	// Calculate the percentage of positive feedback
+	// Check if 'all' is zero to avoid division by zero
+	const positive = all === 0 ? 0 : (good / all) * 100;
+
 	return (
 		<div>
 			<h1>give feedback</h1>
-
 			<button onClick={() => setGood(good + 1)}>good</button>
 			<button onClick={() => setNeutral(neutral + 1)}>neutral</button>
 			<button onClick={() => setBad(bad + 1)}>bad</button>
 
 			<h1>statistics</h1>
-
 			<div>good {good}</div>
 			<div>neutral {neutral}</div>
 			<div>bad {bad}</div>
+			<div>all {all}</div>
+			<div>average {average}</div>
+			<div>positive {positive} %</div>
 		</div>
 	);
 };
