1f753afba4a1a2416adf1134df389a9bc13baf15
Martin Winther <martin.winther@protonmail.com>
2023-11-04 08:38:55 +0100

    phonebook init

--- part2/coursenotes/src/App.jsx
+++ part2/coursenotes/src/App.jsx
@@ -4,7 +4,9 @@ import Note from "./components/Note";
 
 const App = (props) => {
 	const [notes, setNotes] = useState(props.notes);
-	const [newNote, setNewNote] = useState(" a new note...");
+	const [newNote, setNewNote] = useState("");
+	const [showAll, setShowAll] = useState(true);
+
 	const addNote = (event) => {
 		event.preventDefault();
 		const noteObject = {
@@ -19,11 +21,16 @@ const App = (props) => {
 		console.log(event.target.value);
 		setNewNote(event.target.value);
 	};
+
+	const notesToShow = showAll
+		? notes
+		: notes.filter((note) => note.important === true);
+
 	return (
 		<div>
 			<h1>Notes</h1>
 			<ul>
-				{notes.map((note) => (
+				{notesToShow.map((note) => (
 					<Note key={note.id} note={note} />
 				))}
 			</ul>
