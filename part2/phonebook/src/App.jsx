0bfd2d69fb6d5e88076b0f7974e7254ac395dd70
Martin Winther <martin.winther@protonmail.com>
2023-11-07 09:06:38 +0100

    2.8

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 const App = (props) => {
 	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
 	const [newName, setNewName] = useState("");
+	const [newNumber, setNewNumber] = useState("");
 
 	const checkDuplicate = (name) => {
 		return persons.some((person) => person.name === name);
@@ -19,6 +20,7 @@ const App = (props) => {
 
 		const nameObject = {
 			name: newName,
+			number: newNumber,
 		};
 		setPersons(persons.concat(nameObject));
 		setNewName("");
@@ -28,20 +30,33 @@ const App = (props) => {
 		setNewName(event.target.value);
 	};
 
+	const handleNumberChange = (event) => {
+		setNewNumber(event.target.value);
+	};
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
 			<form onSubmit={addName}>
 				name:
 				<input value={newName} onChange={handleNameChange} />
+				<input value={newNumber} onChange={handleNumberChange} />
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			<ul>
+			<ul
+				style={{
+					listStyleType: "none",
+					padding: 0,
+					margin: 0,
+					textAlign: "left",
+				}}>
 				{persons.map((person, index) => (
-					<li key={index}>{person.name}</li>
+					<li key={index}>
+						{person.name} {person.number}
+					</li>
 				))}
 			</ul>
 		</div>
