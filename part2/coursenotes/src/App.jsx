840bec26140cec04e5f3daeae78e39ca97507d85
Martin Winther <martin.winther@protonmail.com>
2023-11-18 08:25:16 +0100

    error message and footer

--- part2/coursenotes/src/App.jsx
+++ part2/coursenotes/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import Note from "./components/Note";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 import noteService from "./services/notes";
 
 const App = () => {
 	const [notes, setNotes] = useState([]);
 	const [newNote, setNewNote] = useState("");
-	const [showAll, setShowAll] = useState(false);
+	const [showAll, setShowAll] = useState(true);
+	const [errorMessage, setErrorMessage] = useState(null);
 
 	useEffect(() => {
 		noteService.getAll().then((initialNotes) => {
@@ -28,7 +30,6 @@ const App = () => {
 	};
 
 	const toggleImportanceOf = (id) => {
-		const url = `http://localhost:3001/notes/${id}`;
 		const note = notes.find((n) => n.id === id);
 		const changedNote = { ...note, important: !note.important };
 
@@ -38,8 +39,12 @@ const App = () => {
 				setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)));
 			})
 			.catch((error) => {
-				alert(`the note '${note.content}' was already deleted from server`);
-				setNotes(notes.filter((n) => n.id !== id));
+				setErrorMessage(
+					`Note '${note.content}' was already removed from server`
+				);
+				setTimeout(() => {
+					setErrorMessage(null);
+				}, 5000);
 			});
 	};
 
@@ -52,6 +57,7 @@ const App = () => {
 	return (
 		<div>
 			<h1>Notes</h1>
+			<Notification message={errorMessage} />
 			<div>
 				<button onClick={() => setShowAll(!showAll)}>
 					show {showAll ? "important" : "all"}
@@ -70,6 +76,7 @@ const App = () => {
 				<input value={newNote} onChange={handleNoteChange} />
 				<button type="submit">save</button>
 			</form>
+			<Footer />
 		</div>
 	);
 };
