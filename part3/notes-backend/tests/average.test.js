5896f2bad38040c7299a28056c143d5ad89d9486
Martin Winther <martin.winther@protonmail.com>
2024-06-10 08:15:28 +0200

    replace jest with test

--- part3/notes-backend/tests/average.test.js
+++ part3/notes-backend/tests/average.test.js
@@ -1,15 +1,18 @@
+const { test, describe } = require('node:test')
+const assert = require('node:assert')
+
 const average = require('../utils/for_testing').average
 
 describe('average', () => {
-  test('of one value is the value itself', () => {
-    expect(average([1])).toBe(1)
-  })
+	test('of one value is the value itself', () => {
+		assert.strictEqual(average([1]), 1)
+	})
 
-  test('of many is calculated right', () => {
-    expect(average([1, 2, 3, 4, 5, 6])).toBe(3.5)
-  })
+	test('of many is calculated right', () => {
+		assert.strictEqual(average([1, 2, 3, 4, 5, 6]), 3.5)
+	})
 
-  test('of empty array is zero', () => {
-    expect(average([])).toBe(0)
-  })
+	test('of empty array is zero', () => {
+		assert.strictEqual(average([]), 0)
+	})
 })
