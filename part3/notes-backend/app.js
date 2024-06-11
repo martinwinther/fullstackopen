dcbe659760d2c3f7ab42c9d9658d0fe10b3d15ed
Martin Winther <martin.winther@protonmail.com>
2024-06-11 07:14:14 +0200

    start exercises

--- part3/notes-backend/app.js
+++ part3/notes-backend/app.js
@@ -12,13 +12,13 @@ mongoose.set('strictQuery', false)
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose
-  .connect(config.MONGODB_URI)
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
+	.connect(config.MONGODB_URI)
+	.then(() => {
+		logger.info('connected to MongoDB')
+	})
+	.catch((error) => {
+		logger.error('error connection to MongoDB:', error.message)
+	})
 
 app.use(cors())
 app.use(express.static('dist'))
