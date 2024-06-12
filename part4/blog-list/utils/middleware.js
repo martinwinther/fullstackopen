1705169c5e1e3bc21f1c0405f1da11db2d31435b
Martin Winther <martin.winther@protonmail.com>
2024-06-12 07:12:20 +0200

    add mongoose

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -1,20 +1,29 @@
 const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+	response.status(404).send({ error: 'unknown endpoint' })
+}
+
+const requestLogger = (request, response, next) => {
+	logger.info('Method:', request.method)
+	logger.info('Path:  ', request.path)
+	logger.info('Body:  ', request.body)
+	logger.info('---')
+	next()
 }
 
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
+	console.error(error.message)
 
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
+	if (error.name === 'CastError') {
+		return response.status(400).send({ error: 'malformatted id' })
+	} else if (error.name === 'ValidationError') {
+		return response.status(400).json({ error: error.message })
+	}
 
-  next(error)
+	next(error)
 }
 
 module.exports = {
-  unknownEndpoint,
-  errorHandler,
+	unknownEndpoint,
+	requestLogger,
+	errorHandler,
 }
